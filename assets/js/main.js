c5bec7704592e5f8f9afbf19d9725fd1a378e968
vijayraman96 <vijayadaran@gmail.com>
2020-06-15 16:58:06 +0530

    drag and drop in slide1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,49 @@
-
-$(document).ready(function(){
-
-    function init(){
-        $('#draggable').draggable({
-   
-        });
+    var slideIndex = 1;
+    showSlides(slideIndex);
+    
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
     }
+    
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("container-block1");
+    
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+    
+      slides[slideIndex-1].style.display = "block";  
+     
+    }
+  
+ $('#draggable'+slideIndex).draggable({
+    // stop: function (event, ui) {
+    //     var target = document.querySelector('#draggable'+slideIndex);
+    //     target.style.top = 0
+    //     target.style.left = 0
+    //     target.style.right = 0
+    //     target.style.bottom = 0
+    //     target.style.opacity=1
+    // },
+    drag: function (event, ui) {
+        var target = document.querySelector('#draggable'+slideIndex);
+        target.style.opacity=0.5;
+    }
+});
+
    
-    $('#droppable').droppable({
-      
+    $('#droppable'+slideIndex).droppable({
+  drop: function(ui,event){
+     $('.block1b-image').attr('src','assets/img/Airbone_Character/Before/Character_Long Sleeve-03.svg');
+     $('#draggable'+slideIndex).hide();
+     $('.block1b-text-container'+slideIndex).css('display','block')
+  }
+
     })
-})
