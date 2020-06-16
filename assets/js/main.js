0ef5947fafd344cf6acdebf4c6713833f777f87e
abisheksrm <abisheksrm@gmail.com>
2020-06-16 15:06:13 +0530

    chages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,97 +1,102 @@
-    var slideIndex = 1;
-    showSlides(slideIndex);
-    
-    function plusSlides(n) {
-       
-      showSlides(slideIndex += n);
-    }
-    
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("container-block1");
-    
-      if (n > slides.length) {slideIndex = 1}    
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";  
-      }
-    
-      slides[slideIndex-1].style.display = "block";  
-     
-    }
+var slideIndex = 1;
+showSlides(slideIndex);
 
- 
-    $('#draggable'+slideIndex).draggable({
-        stop: function (event, ui) {
-            var target = document.querySelector('#draggable'+slideIndex);
-            target.style.top = 0
-            target.style.left = 0
-            target.style.right = 0
-            target.style.bottom = 0
-            target.style.opacity=1
-        },
-        drag: function (event, ui) {
-            var target = document.querySelector('#draggable'+slideIndex);
-            target.style.opacity=0.5;
-        }
-    });
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-    $('#draggable2').draggable({
-        stop: function (event, ui) {
-            var target = document.querySelector('#draggable'+slideIndex);
-            target.style.top = 0
-            target.style.left = 0
-            target.style.right = 0
-            target.style.bottom = 0
-            target.style.opacity=1
-        },
-        drag: function (event, ui) {
-            var target = document.querySelector('#draggable'+slideIndex);
-            target.style.opacity=0.5;
-        }
-    });
-    $('#draggable3').draggable({
-      stop: function (event, ui) {
-          var target = document.querySelector('#draggable'+slideIndex);
-          target.style.top = 0
-          target.style.left = 0
-          target.style.right = 0
-          target.style.bottom = 0
-          target.style.opacity=1
-      },
-      drag: function (event, ui) {
-          var target = document.querySelector('#draggable'+slideIndex);
-          target.style.opacity=0.5;
-      }
-  });
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("container-block1");
 
-   
-    $('#droppable'+slideIndex).droppable({
-  drop: function(ui,event){
-     $('.block1b-image').attr('src','assets/img/Airbone_Character/Before/Character_Long Sleeve-03.svg');
-     $('#draggable'+slideIndex).hide();
-     $('.block1b-text-container'+slideIndex).css('display','block')
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
 
-    });
-    $('#droppable2').droppable({
-        drop: function(ui,event){
-           $('.block1b-image').attr('src','assets/img/Airbone_Character/Before/Character_ffp2-03.svg');
-           $('#draggable'+slideIndex).hide();
-           $('.block1b-text-container'+slideIndex).css('display','block')
-        }
-      
-          });
-          $('#droppable3').droppable({
-            drop: function(ui,event){
-               $('.block1b-image').attr('src','assets/img/Airbone_Character/Before/Character_Face shield-03.svg');
-               $('#draggable'+slideIndex).hide();
-               $('.block1b-text-container'+slideIndex).css('display','block')
-            }
-          
-              });
+  slides[slideIndex - 1].style.display = "block";
+}
+
+$("#draggable" + slideIndex).draggable({
+  stop: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+  drag: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.opacity = 0.5;
+  },
+});
+
+$("#draggable2").draggable({
+  stop: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+  drag: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.opacity = 0.5;
+  },
+});
+$("#draggable3").draggable({
+  stop: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+  drag: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.opacity = 0.5;
+  },
+});
+
+$("#droppable" + slideIndex).droppable({
+  drop: function (ui, event) {
+    $(".block1b-image").attr(
+      "src",
+      "assets/img/Airbone_Character/Before/Character_Long Sleeve-03.svg"
+    );
+    $("#draggable" + slideIndex).hide();
+    $(".block1b-text-container" + slideIndex).css("display", "block");
+  },
+});
+$("#droppable2").droppable({
+  drop: function (ui, event) {
+    $(".block1b-image").attr(
+      "src",
+      "assets/img/Airbone_Character/Before/Character_ffp2-03.svg"
+    );
+    $("#draggable" + slideIndex).hide();
+    $(".block1b-text-container" + slideIndex).css("display", "block");
+  },
+});
+$("#droppable3").droppable({
+  drop: function (ui, event) {
+    $(".block1b-image").attr(
+      "src",
+      "assets/img/Airbone_Character/Before/Character_Face shield-03.svg"
+    );
+    $("#draggable" + slideIndex).hide();
+    $(".block1b-text-container" + slideIndex).css("display", "block");
+  },
+});
