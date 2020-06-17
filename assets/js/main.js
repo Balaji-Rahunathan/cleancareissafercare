91f3e2199cf6d105499790af6574a3928efd2c4e
vijayraman96 <vijayadaran@gmail.com>
2020-06-17 14:58:13 +0530

    changes in airborne and droplet.html sites

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,13 +83,49 @@ $("#draggable4").draggable({
     target.style.opacity = 0.5;
   },
 });
+$("#draggable5").draggable({
+ 
+  stop: function (event, ui) {
+   
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+  drag: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.opacity = 0.5;
+    $('.block2b-image-container').css("background-image","url('../assets/img/reverse/Sleeve_3.svg')");
+  },
+ 
+});
+$("#draggable6").draggable({
+ 
+  stop: function (event, ui) {
+   
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.top = 0;
+    target.style.left = 0;
+    target.style.right = 0;
+    target.style.bottom = 0;
+    target.style.opacity = 1;
+  },
+  drag: function (event, ui) {
+    var target = document.querySelector("#draggable" + slideIndex);
+    target.style.opacity = 0.5;
+    $('.block2c-image-container').css("background-image","url('../assets/img/reverse/Sleeve_2.svg')");
+  },
+ 
+});
 $("#droppable" + slideIndex).droppable({
   drop: function (ui, event) {
     $(".block1b-image").attr(
       "src",
       "assets/img/Airbone_Character/Before/Character_Long Sleeve-03.svg"
     );
-    $(".apron-image-container").css("height", "auto");
+    $(".box1").css("height", "330px");
     $("#draggable" + slideIndex).hide();
     $(".block1b-text-container" + slideIndex).css("display", "block");
   },
@@ -100,7 +136,7 @@ $("#droppable2").droppable({
       "src",
       "assets/img/Airbone_Character/Before/Character_ffp2-03.svg"
     );
-    $(".apron-image-container").css("height", "auto");
+    $(".box2").css("height", "210px");
     $("#draggable" + slideIndex).hide();
     $(".block1b-text-container" + slideIndex).css("display", "block");
   },
@@ -111,7 +147,7 @@ $("#droppable3").droppable({
       "src",
       "assets/img/Airbone_Character/Before/Character_Face shield-03.svg"
     );
-    $(".apron-image-container").css("height", "auto");
+    $(".box3").css("height", "210px");
     $("#draggable" + slideIndex).hide();
     $(".block1b-text-container" + slideIndex).css("display", "block");
   },
@@ -121,6 +157,28 @@ $("#droppable4").droppable({
     $(".block1b-image").attr(
       "src",
       "assets/img/Airbone_Character/Before/Balaji Precautions-03.svg"
+    );
+    $(".box4").css("height", "210px");
+    $("#draggable" + slideIndex).hide();
+    $(".block1b-text-container" + slideIndex).css("display", "block");
+  },
+});
+$("#droppable5").droppable({
+  drop: function (ui, event) {
+    $(".apron").attr(
+      "src",
+      "assets/img/Assets/Gloves.svg"
+    );
+    $(".apron-image-container").css("height", "auto");
+    $("#draggable" + slideIndex).hide();
+    $(".block1b-text-container" + slideIndex).css("display", "block");
+  },
+});
+$("#droppable6").droppable({
+  drop: function (ui, event) {
+    $(".apron1").attr(
+      "src",
+      "assets/img/Assets/Face shield.svg"
     );
     $(".apron-image-container").css("height", "auto");
     $("#draggable" + slideIndex).hide();
